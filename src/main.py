9beddf22d1aeb06dea1a758662f2ad9b13a9e762
Master801 <master801@users.noreply.github.com>
2019-08-21 15:49:03 -0500

    Fixed validation of entries before constructing the bin file.
    Added and fixed formatting of messages

--- src/main.py
+++ src/main.py
@@ -31,32 +31,69 @@ def main():
     json_address_offset = read_json_data['address_offset']
     json_addresses = read_json_data['addresses']
 
-    output_file = open(output, 'xb')
-
-    output_file.write('TRANSLATE'.encode('utf8'))  # Header
-
     address_offset = 0x00000000
-    if len(json_address_offset) > 0:
-        if json_address_offset[2:].lower() == '0x':
+    if len(json_address_offset) > 0:  # Validate
+        if json_address_offset[:2].lower() == '0x':
             unpacked_address = json_address_offset[2:]
+            address_offset = int(unpacked_address, 16)
             pass
         else:
             unpacked_address = json_address_offset
+            address_offset = int(unpacked_address)
             pass
-        address_offset = int(unpacked_address, 16)
         pass
+    print('Main address offset: 0x{:08X}\n'.format(address_offset))
 
-    output_file.write(struct.pack('<I', address_offset))  # Address offset
-    output_file.write(struct.pack('<L', len(json_addresses)))  # Amount of entries
-    for i in range(len(json_addresses)):
-        block: dict = json_addresses[i]
-
+    addresses = list()
+    for block in json_addresses:
         offset: str = block['offset']
         text: str = block['text']
 
-        if len(offset) < 1 or len(text) < 1:
-            print('Found invalid line at index {0}\nSkipping block...'.format(i))
+        print('\nValidating block with address offset: {0}...'.format(offset))
+
+        if offset[:2].lower() == '0x':  # Validate
+            unpacked_address = offset[2:]
+            entry_address_offset = int(unpacked_address, 16)
+            pass
+        else:
+            unpacked_address = offset
+            entry_address_offset = int(unpacked_address)
+            pass
+
+        if entry_address_offset < 0:  # Sanity check
+            print('Could not parse offset address! Skipping...')
             continue
+
+        if len(text) < 1:  # Sanity check
+            print('No text was given! Skipping...')
+            continue
+
+        construct = dict()
+        construct.update({'offset': entry_address_offset})
+        construct.update({'text': text})
+        if 'original' in block:
+            construct.update({'original': block['original']})
+            pass
+
+        addresses.append(construct)
+        continue
+
+    print('\n\nConstructing bin file...\n')
+    construct_bin(output, address_offset, addresses)
+    return
+
+
+def construct_bin(output: str, address_offset: int, addresses: list):
+    output_file = open(output, 'xb')
+
+    output_file.write('TRANSLATE'.encode('utf8'))  # Header
+    output_file.write(struct.pack('<I', address_offset))  # Address offset
+    output_file.write(struct.pack('<L', len(addresses)))  # Amount of entries
+    for i in range(len(addresses)):
+        block = addresses[i]
+
+        entry_address_offset: int = block['offset']
+        text: str = block['text']
 
         if 'original' in block:
             orig = 'Original: \"{0}\"\n'.format(block['original'])
@@ -65,30 +102,18 @@ def main():
             orig = '\n'
             pass
 
-        print('Offset: {0}'.format(offset))
+        print('Index: 0x{:04X}'.format(i))
+        print('Address offset: 0x{:08X}'.format(entry_address_offset))
         print('Text: \"{0}\"'.format(text))
         print(orig)
 
         output_file.write(struct.pack('<I', i))  # Write index
-
-        if offset[2:].lower() == '0x':
-            unpacked_address = offset[2:]
-            pass
-        else:
-            unpacked_address = offset
-            pass
-
-        unpacked_address = int(unpacked_address, 16)
-
-        if unpacked_address < 0:
-            print('Invalid offset! Skipping...\n')
-            continue
-
         text_data = text.encode('utf-8')
 
-        output_file.write(struct.pack('<I', unpacked_address))  # Address
+        output_file.write(struct.pack('<I', entry_address_offset))  # Address
         output_file.write(struct.pack('<H', len(text_data)))  # Length of text
         output_file.write(text_data)  # Text
+        # Do not write entry original_text
 
         continue
     output_file.close()
